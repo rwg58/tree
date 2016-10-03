76b1b357d7c0aa2c674122cfe8e442617bc5dfd3
rwg58 <rwg58@mail.missouri.edu>
2016-10-02 23:54:01 -0400

    whitespace

--- pytree.py
+++ pytree.py
@@ -4,38 +4,32 @@ import sys
 import os
 
 # YOUR CODE GOES here
-if len(sys.argv) == 1:  
-    currentPath = os.getcwd()  
-else:  
-    currentPath = sys.argv[1]  
-
-    
 def clean_hidden_files(list):
-    newdirs = [item for item in list if item[0] != '.']
-    return newdirs
+	newdirs = [item for item in list if item[0] != '.']
+	return newdirs
 
 
 def filetree(path, t, has):  
-    has.append(True)  
-    block = '  '  
-    for i in range(t):  
-        if has[i] is True: 
-            block = block + '|  '  
-        else: 
-            block = block + '   '  
-    dirs = os.listdir(path)
-    dirs = clean_hidden_files(dirs)
-    for i in dirs:  
-        tmp = path + os.sep + i  
-        if i == dirs[-1]:  
-            down = '|-'  
-            has[t] = False  
-        else:  
-            down = '|-'  
-            has[t] = True  
-        print(block + down + i)  
-        if os.path.isdir(tmp) :  
-            filetree(tmp, t + 1, has)  
+	has.append(True)  
+	block = '  '  
+	for i in range(t):  
+		if has[i] is True: 
+			block = block + '|  '  
+		else: 
+			block = block + '   '  
+	dirs = os.listdir(path)
+	dirs = clean_hidden_files(dirs)
+	for i in dirs:  
+        	tmp = path + os.sep + i  
+        	if i == dirs[-1]:  
+            		down = '|-'  
+            		has[t] = False  
+        	else:  
+            		down = '|-'  
+            		has[t] = True  
+        	print(block + down + i)  
+        	if os.path.isdir(tmp):  
+            		filetree(tmp, t + 1, has)  
 
 # print (currentPath)  
 # filetree(currentPath, 0, [])  
@@ -44,5 +38,9 @@ def filetree(path, t, has):
 if __name__ == '__main__':
 # just for demo
 # subprocess.run(['tree'] + sys.argv[1:])
-    print(currentPath)  
-    filetree(currentPath, 0, []) 
+	if len(sys.argv) == 1:
+        	currentPath = os.getcwd()  
+	else:  
+        	currentPath = sys.argv[1]
+	print(currentPath)  
+	filetree(currentPath, 0, []) 
