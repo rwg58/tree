f033b457f882ca0724a581e4c46c61b753d8f67a
rwg58 <rwg58@mail.missouri.edu>
2016-10-02 23:31:27 -0400

    fix some problem

--- pytree.py
+++ pytree.py
@@ -9,13 +9,22 @@ if len(sys.argv) == 1:
 else:  
     currentPath = sys.argv[1]  
 
+    
+def clean_hidden_files(list):
+    newdirs = [item for item in list if item[0] != '.']
+    return newdirs
+
+
 def filetree(path, t, has):  
     has.append(True)  
     block = '  '  
     for i in range(t):  
-        if has[i] == True: block += '|  '  
-        else: block += '   '  
-    dirs = os.listdir(path)  
+        if has[i] is True: 
+            block = block + '|  '  
+        else: 
+            block = block + '   '  
+    dirs = os.listdir(path)
+    dirs = clean_hidden_files(dirs)
     for i in dirs:  
         tmp = path + os.sep + i  
         if i == dirs[-1]:  
@@ -24,16 +33,16 @@ def filetree(path, t, has):
         else:  
             down = '|-'  
             has[t] = True  
-        print (block + down + i)  
+        print(block + down + i)  
         if os.path.isdir(tmp) :  
             filetree(tmp, t + 1, has)  
 
-#print (currentPath)  
-#filetree(currentPath, 0, [])  
+# print (currentPath)  
+# filetree(currentPath, 0, [])  
 
 
 if __name__ == '__main__':
-    # just for demo
-#    subprocess.run(['tree'] + sys.argv[1:])
-    print (currentPath)  
+# just for demo
+# subprocess.run(['tree'] + sys.argv[1:])
+    print(currentPath)  
     filetree(currentPath, 0, []) 
